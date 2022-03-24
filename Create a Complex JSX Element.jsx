5d3d99a089123512dfeadb5c3cb8e7a585c5c450
Denis Shubin <denis.shubin@gmail.com>
2022-03-25 00:23:11 +0300

    Create a Complex JSX Element

--- Create a Complex JSX Element.jsx
+++ Create a Complex JSX Element.jsx
@@ -1,9 +1,14 @@
-const JSX = <div>
+const JSX = (
+  <div>
     <h1>Create a Complex JSX Element</h1>
-<p>The last challenge was a simple example of JSX, but JSX can represent more complex HTML as well.</p>
-<ul>
-<li>Paragraph One</li>
-<li>Paragraph Two</li>
-<li>Paragraph Three</li>
-</ul>
-</div>
+    <p>
+      The last challenge was a simple example of JSX, but JSX can represent more
+      complex HTML as well.
+    </p>
+    <ul>
+      <li>Paragraph One</li>
+      <li>Paragraph Two</li>
+      <li>Paragraph Three</li>
+    </ul>
+  </div>
+);
